a39a261698f7c89e8568edc9945dc7ab1f9f5fc8
Tiiatoiv <tiiatoiv@metropolia.fi>
2021-04-21 22:50:27 +0300

    projectstart stuff

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,7 @@
 import animalSchema from './animalSchema.js';
 import speciesSchema from './speciesSchema.js';
 import categorySchema from './categorySchema.js';
+import gameStatSchema from './gameStatSchema';
 import {gql} from 'apollo-server-express';
 
 const linkSchema = gql`
@@ -17,4 +18,5 @@ export default [
   animalSchema,
   speciesSchema,
   categorySchema,
+  gameStatSchema,
 ];
