f9f521c782778f4997506a81f3f64b1ee9225bf5
Tiiatoiv <tiiatoiv@metropolia.fi>
2021-04-21 22:51:07 +0300

    projectstart stuff

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,5 +1,6 @@
 import animalResolver from './animalResolver.js';
 import speciesResolver from './speciesResolver.js';
 import categoryResolver from './categoryResolver.js';
+import GameStatResolver from './gameStatResolver.js';
 
 export default [animalResolver, speciesResolver, categoryResolver];
