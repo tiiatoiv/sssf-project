22e2de1454f27138deaf9b9e3fb7b7f1fc8ba663
Tiiatoiv <tiiatoiv@metropolia.fi>
2021-04-21 21:46:44 +0300

    starting stuff for project

---
+++ resolvers/gameStatResolver.js
@@ -0,0 +1,23 @@
+import Animal from '../models/animal.js';
+
+export default {
+  Query: {
+    gamestats: (parent, args) => {
+      return GameStat.find();
+    },
+    angamestat: (parent, args) => {
+      return GameStat.findById(args.id);
+    }
+  },
+  Mutation: {
+    addGameStat: (parent, args) => {
+      console.log('GameStatResolver, addGameStat', args);
+      const newGameStat = new GameStat(args);
+      return newGameStat.save();
+    },
+    modifyGameStat: (parent, args) => {
+      console.log('GameStatResolver, modifyGameStat', args);
+      return GameStat.findByIdAndUpdate(args.id, args);
+    },
+  }
+};
