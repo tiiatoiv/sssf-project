8b3b555828a2cf13d691814ed07361bd3735afbb
Tiiatoiv <tiiatoiv@metropolia.fi>
2021-04-26 16:44:07 +0300

    working version

--- resolvers/index.js
+++ resolvers/index.js
@@ -3,6 +3,5 @@ import speciesResolver from './speciesResolver.js';
 import categoryResolver from './categoryResolver.js';
 import gameStatResolver from './gameStatResolver.js';
 import mapResolver from './mapResolver.js';
-import userResolver from './userResolver.js';
 
-export default [animalResolver, speciesResolver, categoryResolver, gameStatResolver, mapResolver, userResolver];
+export default [animalResolver, speciesResolver, categoryResolver, gameStatResolver, mapResolver];
--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,6 @@ import animalSchema from './animalSchema.js';
 import speciesSchema from './speciesSchema.js';
 import categorySchema from './categorySchema.js';
 import gameStatSchema from './gameStatSchema.js';
-import userSchema from './userSchema.js';
 import mapSchema from './mapSchema.js';
 import {gql} from 'apollo-server-express';
 
